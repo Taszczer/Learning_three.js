3c9aa7197d910e99b79096d4dcd1906fc4cf6293
lolkek1234567890 <dima2019mega@gmail.com>
2023-05-20 18:05:16 +0200

    first try

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+
+import { styles } from "../styles"
+import { NavLinks } from "../constants"
+import { logo, menu, close} from "../assets"
+
+const Navbar = () => {
+  return (
+    <nav className={`${styles.paddingX}`}>
+
+    </nav>
+  )
+}
+
+export default Navbar
