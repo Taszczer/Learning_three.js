3830d9c44d21119ff16bb6b975940dc9330c55d0
lolkek1234567890 <dima2019mega@gmail.com>
2023-05-22 19:49:21 +0200

    apdate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,12 @@ const Navbar = () => {
   
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0  z-20 bg-primary
+    <nav className={`${
+      styles.paddingX} 
+      w-full flex items-center py-5 fixed top-0 z-20 bg-primary 
     `}
     >
-     <div className='w-full flex justify-between items-center max-w-7xl mx-auto'></div> 
+     <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
       <Link to="/" 
       className='flex items-center gap-2'
       onClick={() =>{
@@ -23,10 +25,26 @@ const Navbar = () => {
       }}
       > 
       <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-      <p className=' text-white text-[18px] font-bold cursor-pointer flex'> Dima  
-      <span className='sm:block hidden'>\/ Three.js student</span>
+      <p className=' text-white text-[18px] font-bold cursor-pointer flex'> Dima _ <span className='sm:block hidden'>  Three.js student</span>
       </p>
       </Link>
+
+        <ul className='list-none hidden sm:flex flex-row  gap-10 '>
+          {navLinks.map((link) =>(
+            <li 
+            key={link.id}
+            className={`${
+              active === link.title
+              ? "text-white"
+              : "text-secondary"
+            } hover:text-white text-[18px] font-medium cursor-pointer `}
+            onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div> 
     </nav>///amogus
   )
 }
