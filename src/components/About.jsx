abb2de568a5e3ea985df9cc60ba4c6b25f006713
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-11 14:38:09 +0200

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,27 @@ import { fadeIn, textVariant } from '../utils/motion'
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full"> {title} </Tilt>
+    <Tilt className="xs:w-[250px] w-full">
+       <motion.div 
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+       > 
+      <div
+       options={{
+        max:45,
+        scale: 1,
+        speed:450
+       }}
+       className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
+       flex justify-evenly items-center flex-col '
+      >
+        <img src={icon} alt={title} 
+        className='w-16 h-16 object-contain'
+        /> 
+        <h3 className='text-white font-bold text-[20px] text-center'>{title}</h3>
+      </div>
+       </motion.div>
+    </Tilt>
   )
 }
 
