0adf0081842f844046cb9e0605d3c7b934bd69a9
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-14 23:34:49 +0200

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import {motion} from 'framer-motion'
+
+import 'react-vertical-timeline-component/style.min.css'
 
 const Experience = () => {
   return (
