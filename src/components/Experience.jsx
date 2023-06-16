ca94d3e1118f58533651945f145695d4669b55ff
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-16 21:34:42 +0200

    animation fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,9 +4,21 @@ import {motion} from 'framer-motion'
 
 import 'react-vertical-timeline-component/style.min.css'
 
+import {styles} from '../styles'
+import {experiences} from '../constants'
+
+import {textVariant} from '../utils/motion'
+
 const Experience = () => {
   return (
-    <div></div>
+    <>
+    <motion.div
+    variants={textVariant}
+    >
+
+
+    </motion.div>
+    </>
   )
 }
 
