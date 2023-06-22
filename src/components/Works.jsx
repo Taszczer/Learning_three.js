6fc765ae7ad252af8576c7c1e4fb8a886e1a3fcd
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-22 13:11:21 +0200

    starting works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,13 +1,54 @@
 import { Tilt } from "react-tilt"
-import { Motion } from "react-motion"
+import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { github } from "../assets"
 import { SectionWrapper } from "../hoc"
 import { projects } from "../constants"
 import { fadeIn, textVariant } from "../utils/motion"
 
-const Works = () => {
-  return <></>
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      ></Tilt>
+    </motion.div>
+  )
 }
 
-export default Works
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I created</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          In this section there are projects that I made myself with friends
+        </motion.p>
+      </div>
+
+      {/* <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((projects, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div> */}
+    </>
+  )
+}
+
+export default SectionWrapper(Works, "")
