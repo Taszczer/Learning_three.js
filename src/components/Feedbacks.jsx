8dc4783f807773a6e23f02cc6892b799cd4bfcfb
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-24 17:53:49 +0200

    feedbacks

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React from "react"
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { SectionWrapper } from "../hoc"
+import { fadeIn, textVariant } from "../utils/motion"
+import { testimonials } from "../constants"
+
+const FeedbackCard = ({
+  index,
+  testimonial,
+  name,
+  designation,
+  company,
+  image,
+}) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+  >
+    <p className="text-white font-black text-[48px]">"</p>
+
+    <div className="mt-1">
+      <p>{testimonial}</p>
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex flex-1 flex-col">
+          <p><span>@</span> { name }</p>
+          <p>{ designation } of { company }</p>
+        </div>
+      </div>
+    </div>   
+
+  </motion.div>
+)
 
 const Feedbacks = () => {
-  return <div>Feedbacks</div>;
-}; //amogus
+  return (
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div
+        className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
+      >
+        <motion.div variants={textVariant}>
+          <p className={styles.sectionSubText}>What other say</p>
+          <h2 className={styles.sectionHeadText}>Testimonails</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  )
+} //amogus
 
-export default Feedbacks;
+export default Feedbacks
