d9594792b9bdd2b1637d50b09a33e06f50824b0c
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-25 20:12:06 +0200

    small earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Suspense } from "react"
+import { useGLTF, OrbitControls, Preload } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+
+import CanvasLoader from "../Loader"
 
 const Earth = () => {
-  return <div>Earth</div>;
-};
+  const earth = useGLTF("./planet/scene.gltf")
 
-export default Earth;
+  return <primitive object={earth.scene} />
+}
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas> //Polar for rotating
+  )
+}
+
+export default EarthCanvas
