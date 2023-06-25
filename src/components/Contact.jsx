b355f8090c4006ea5d5b268d67036d8076cc2b14
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-25 18:28:01 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from "react"
+import { useState, useRef } from "react"
+import { motion } from "framer-motion"
+import emailjs from "@emailjs/browser"
+
+import { styles } from "../styles"
+import { EarthCanvas } from "../canvas"
+import { SectionWrapper } from "../hoc"
+import { slideIn } from "../utils/motion"
 
 const Contact = () => {
-  return <div>Contact</div>;
-};
+  const [form, useForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
 
-export default Contact;
+  return <div>Contact</div>
+}
+
+export default Contact
